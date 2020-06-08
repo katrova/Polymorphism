bbf7ff8c017df2f0fb0a835c2a29a434fd298c7c
Kate <Kate@MacBook-Pro-Catherine.local>
2020-06-08 23:00:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/*
+ * Classname : Polymorphism
+ *
+ * 8 June 2020
+ *
+ * Copyright Vasylkivska Kateryna KNTEU
+ *
+ * Module 2 task 3
+ *
+ * Create and implement  two interfaces for your class.
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
